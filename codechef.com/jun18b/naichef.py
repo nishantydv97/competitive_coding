daa7311dc5c4580faf8ece816304f0c43f869517
nishantydv97 <phy.nishant@gmail.com>
2018-06-29 12:34:09 +0530

    jun challenge added

--- codechef.com/jun18b/naichef.py
+++ codechef.com/jun18b/naichef.py
@@ -1,3 +1,5 @@
+'''
+
 Once, after a stressful day, Chef decided to relax and visit a casino near his house to gamble. He feels lucky and he's going to bet almost all of his money.
 
 The game Chef is going to play in the casino consists of tossing a die with N faces twice. There is a number written on each face of the die (these numbers are not necessarily distinct). In order to win, Chef must get the number A on the first toss and the number B on the second toss of the die.
@@ -34,6 +36,8 @@ Example Output
 1.0000000000
 0.2500000000
 
+
+'''
 
 
 import math
