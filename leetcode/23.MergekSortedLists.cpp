9de1fad2a26a2947e6fe2e62538bd9e3de25a7d8
Nishant Kumar <phy.nishant@gmail.com>
2020-09-10 03:31:16 +0530

    Create 23.MergekSortedLists.cpp

---
+++ leetcode/23.MergekSortedLists.cpp
@@ -0,0 +1,70 @@
+/*
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode *q=NULL;
+        ListNode *ans=q;
+        //cout<<ans;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(ListNode *i : lists){
+            ListNode* p=i;
+            while(p!=NULL){
+                pq.push(p->val);
+                p=p->next;
+            }
+        }
+        while(!pq.empty()){
+            if(q==NULL){
+                q=new ListNode(pq.top());
+                ans=q;
+            }else{
+                q->next=new ListNode(pq.top());
+                q=q->next;
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};
