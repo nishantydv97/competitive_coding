d6db7273d04a367b731f71d79d70b1495769c1fc
Nishant Kumar <phy.nishant@gmail.com>
2020-09-12 00:20:54 +0530

    Create 653.TwoSumIV-InputIsABST.cpp

---
+++ leetcode/653.TwoSumIV-InputIsABST.cpp
@@ -0,0 +1,56 @@
+/*
+
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+Example 1:
+
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+ 
+
+Example 2:
+
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 28
+
+Output: False
+
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    set<int> st; 
+    //st.insert(0);
+    bool findTarget(TreeNode* root, int k) {
+        if(root==NULL) return false;
+        if(st.find(k-root->val)!=st.end()) return true;
+        st.insert(root->val);
+        return findTarget(root->left,k) or findTarget(root->right,k);
+    }
+};
