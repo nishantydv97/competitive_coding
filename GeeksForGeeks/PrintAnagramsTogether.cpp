6d160cd0f9f4d2a887c6582303c421674e79233b
Nishant Kumar <phy.nishant@gmail.com>
2021-01-28 00:20:04 +0530

    Create PrintAnagramsTogether.cpp

---
+++ GeeksForGeeks/PrintAnagramsTogether.cpp
@@ -0,0 +1,103 @@
+/*
+
+Given an array of strings, return all groups of strings that are anagrams. The groups must be created in order of their appearance in the original array. Look at the sample case for clarification.
+
+
+Example 1:
+
+Input:
+N = 5
+words[] = {act,god,cat,dog,tac}
+Output: 
+god dog
+act cat tac
+Explanation:
+There are 2 groups of
+anagrams "god", "dog" make group 1.
+"act", "cat", "tac" make group 2.
+Example 2:
+
+Input:
+N = 3
+words[] = {no,on,is}
+Output: 
+no on
+is
+Explanation:
+There are 2 groups of
+anagrams "no", "on" make group 1.
+"is" makes group 2. 
+
+Your Task:
+The task is to complete the function Anagrams() that takes a list of strings as input and returns a list of groups such that each group consists of all the strings that are anagrams.
+
+
+Expected Time Complexity: O(N*|S|*log|S|), where |S| is the length of the strings.
+Expected Auxiliary Space: O(N*|S|), where |S| is the length of the strings.
+
+
+
+
+*/
+
+
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+vector<vector<string> > Anagrams(vector<string>& string_list) ;
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+vector<vector<string> > Anagrams(vector<string>& string_list) 
+{
+    unordered_map<string, vector<string>> ump;
+    for(auto i : string_list){
+        string tmp=i;
+        sort(tmp.begin(), tmp.end());
+        if(ump.find(tmp)==ump.end()){
+            ump[tmp].push_back(i);
+        }else{
+            ump[tmp].push_back(i);
+        }
+    }
+    vector<vector<string>> ans;
+    for(auto i : ump){
+        ans.push_back(i.second);
+    }
+    return ans;
+    // Your Code Here
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        vector<vector<string> > result = Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
