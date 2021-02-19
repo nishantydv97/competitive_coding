833e9329b8ea55d020a7e7598bb544df69545d0b
Nishant Kumar <phy.nishant@gmail.com>
2021-02-20 02:50:46 +0530

    Create 1249.MinimumRemoveToMakeValidParentheses.cpp

---
+++ leetcode/1249.MinimumRemoveToMakeValidParentheses.cpp
@@ -0,0 +1,77 @@
+/*
+
+
+Given a string s of '(' , ')' and lowercase English characters. 
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+Example 4:
+
+Input: s = "(a(b(c)d)"
+Output: "a(b(c)d)"
+ 
+
+Constraints:
+
+1 <= s.length <= 10^5
+s[i] is one of  '(' , ')' and lowercase English letters.
+
+
+
+
+*/
+
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<char> stk;
+        stack<int> dfct;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='(' and s[i]!=')') continue;
+            if(stk.empty()){
+                stk.push(s[i]);
+                dfct.push(i);
+            }else{
+                if(stk.top()=='(' and s[i]==')'){
+                    stk.pop();
+                    dfct.pop();
+                }else{
+                    stk.push(s[i]);
+                    dfct.push(i);
+                }
+            }
+        }
+        vector<int> v(s.size(), 1);
+        while(!dfct.empty()){
+            v[dfct.top()]=0;
+            dfct.pop();
+        }
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(v[i]) ans+=s[i];
+        }
+        return ans;
+    }
+};
