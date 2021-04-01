83e0884122ada5bc185b8639502d3215e3f9b15a
Nishant Kumar <phy.nishant@gmail.com>
2021-04-01 08:43:41 +0530

    Create NutsAndBoltsProblem.cpp

---
+++ GeeksForGeeks/NutsAndBoltsProblem.cpp
@@ -0,0 +1,106 @@
+/*
+
+Given a set of N nuts of different sizes and N bolts of different sizes. There is a one-one mapping between nuts and bolts. Match nuts and bolts efficiently.
+
+Comparison of a nut to another nut or a bolt to another bolt is not allowed. It means nut can only be compared with bolt and bolt can only be compared with nut to see which one is bigger/smaller.
+The elements should follow the following order ! # $ % & * @ ^ ~ .
+
+Example 1:
+
+Input: 
+N = 5
+nuts[] = {@, %, $, #, ^}
+bolts[] = {%, @, #, $ ^}
+Output: 
+# $ % @ ^
+# $ % @ ^
+Example 2:
+
+Input: 
+N = 9
+nuts[] = {^, &, %, @, #, *, $, ~, !}
+bolts[] = {~, #, @, %, &, *, $ ,^, !}
+Output: 
+! # $ % & * @ ^ ~
+! # $ % & * @ ^ ~
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function matchPairs() which takes an array of characters nuts[], bolts[] and n as parameters and returns void. You need to change the array itswelf.
+
+Expected Time Complexity: O(NlogN)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 9
+Array of Nuts/Bolts can only consist of the following elements:{'@', '#', '$', '%', '^', '&', '~', '*', '!'}.
+
+
+*/
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+    int partition(char *arr, int low, int high, int pivit){
+        int i=low;
+        for(int j=low; j<high; j++){
+            if(arr[j]<pivit){
+                swap(arr[i], arr[j]);
+                i++;
+            }else if(arr[j]==pivit){
+                swap(arr[high], arr[j]);
+                j--;
+            }
+        }
+        swap(arr[i], arr[high]);
+        return i;
+    }
+    void quickSort(char *nuts, char *bolts, int low, int high){
+        if(low<high){
+            int part1=partition(nuts, low, high, bolts[high]);
+            int part2=partition(bolts, low, high, nuts[part1]);
+            quickSort(nuts, bolts, low, part1-1);
+            quickSort(nuts, bolts, part1+1, high);
+        }
+    }
+	void matchPairs(char nuts[], char bolts[], int n) {
+	    quickSort(nuts, bolts, 0, n-1);
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(nuts, bolts, n);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
