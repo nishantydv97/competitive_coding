e72864f0a2a730a4c0393579ff6c7be00a048f58
Nishant Kumar <phy.nishant@gmail.com>
2022-01-26 01:26:29 +0530

    Create 941.ValidMountainArray.java

---
+++ leetcode/941.ValidMountainArray.java
@@ -0,0 +1,57 @@
+/*
+
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+ 
+
+Example 1:
+
+Input: arr = [2,1]
+Output: false
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 104
+Accepted
+243,254
+Submissions
+733,352
+
+
+*/
+
+
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length<=2) return false;
+
+        int i=1;
+        while (i<arr.length){
+            if(arr[i-1]>=arr[i]) break;
+            i++;
+        }
+        if(i>=arr.length || i==1) return false;
+        while(i<arr.length){
+            if(arr[i-1]<=arr[i]) return false;
+            i++;
+        }
+        return true;
+    }
+}
