14821c5c1a6a4ec863d76c91cff6078c17859581
Nishant Kumar <phy.nishant@gmail.com>
2022-01-30 03:30:17 +0530

    Create 84.LargestRectangleInHistogram.java

---
+++ leetcode/84.LargestRectangleInHistogram.java
@@ -0,0 +1,79 @@
+/*
+
+
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+
+
+*/
+
+class Solution {
+    
+    public static int[] prevSmallerIndex(int[] heights){
+        int[] prevSmallerIndex = new int[heights.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<heights.length; i++){
+            while(!stack.empty() && heights[stack.peek()]>= heights[i]){
+                stack.pop();
+            }
+            if(stack.empty()){
+                prevSmallerIndex[i]=-1;
+            }else{
+                prevSmallerIndex[i]=stack.peek();
+            }
+            stack.push(i);
+        }
+        return prevSmallerIndex;
+    }
+
+    public static int[] nextSmallerIndex(int[] heights){
+        int[] nextSmallerIndex = new int[heights.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = heights.length-1; i >= 0; i--){
+            while(!stack.empty() && heights[stack.peek()]>= heights[i]){
+                stack.pop();
+            }
+            if(stack.empty()){
+                nextSmallerIndex[i]=heights.length;
+            }else{
+                nextSmallerIndex[i]=stack.peek();
+            }
+            stack.push(i);
+        }
+        return nextSmallerIndex;
+
+    }
+    
+    public int largestRectangleArea(int[] heights) {
+        int maxArea=0;
+        int[] prevSmallerIndex = prevSmallerIndex(heights);
+        int[] nextSmallerIndex = nextSmallerIndex(heights);
+        // Arrays.stream(prevSmallerIndex).forEach(i -> System.out.print(i+" "));
+        // System.out.println();
+        // Arrays.stream(nextSmallerIndex).forEach(i -> System.out.print(i+" "));
+        for(int i = 0; i < heights.length; i++){
+            maxArea=Math.max(maxArea, (nextSmallerIndex[i]-prevSmallerIndex[i]-1)*heights[i]);
+        }
+        return maxArea;
+    }
+}
