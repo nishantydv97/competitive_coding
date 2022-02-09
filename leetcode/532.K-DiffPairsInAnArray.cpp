325ce9b9fab44cfc6801dbc23069e8294723a0d3
Nishant Kumar <phy.nishant@gmail.com>
2022-02-09 17:09:08 +0530

    Create 532.K-DiffPairsInAnArray.cpp

---
+++ leetcode/532.K-DiffPairsInAnArray.cpp
@@ -0,0 +1,63 @@
+/*
+
+
+
+Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+
+A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+
+0 <= i < j < nums.length
+|nums[i] - nums[j]| == k
+Notice that |val| denotes the absolute value of val.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,4,1,5], k = 2
+Output: 2
+Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+Although we have two 1s in the input, we should only return the number of unique pairs.
+Example 2:
+
+Input: nums = [1,2,3,4,5], k = 1
+Output: 4
+Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+Example 3:
+
+Input: nums = [1,3,1,5,4], k = 0
+Output: 1
+Explanation: There is one 0-diff pair in the array, (1, 1).
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-107 <= nums[i] <= 107
+0 <= k <= 107
+Accepted
+215,502
+Submissions
+559,383
+
+
+*/
+
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        unordered_map<int, int> ump;
+        for(auto i : nums){
+            ump[i]++;
+        }
+        int cnt=0;
+        for(auto i : ump){
+          if(ump.find(i.first+k)!=ump.end())
+              if((k==0 and i.second>1) or (k!=0))
+                  cnt++;
+    
+        }
+        return cnt;
+    }
+};
